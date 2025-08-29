f5b1b6415fcf5b1f08c25754f70204c534680abc
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-08-29 13:25:14 +0530

    Created Build Prompt Function

--- EmailWritter/src/main/java/com/email/writer/Service/EmailGeneratorServiceIMPL.java
+++ EmailWritter/src/main/java/com/email/writer/Service/EmailGeneratorServiceIMPL.java
@@ -3,11 +3,33 @@ package com.email.writer.Service;
 import com.email.writer.Entity.EmailRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Service
 public class EmailGeneratorServiceIMPL implements EmailGeneratorService {
     public String generateEmailRequest(EmailRequest emailRequest) {
         //buuild prompt for the Gemini API
+        String prompt = buildPrompt(emailRequest);
         //craft a request in the format
+        Map<String,Object> requestBody = Map.of(
+                "contents",new Object[]{
+                        Map.of("parts",new Object[]{
+
+                        })
+                }
+        );
+        //do request
+        //return respones
+    }
+
+    private String buildPrompt(EmailRequest emailRequest) {
+        StringBuilder prompt= new StringBuilder();
+        prompt.append("Generate a professional Email Reply for the following content. Please don't generate a subject line");
+        if(emailRequest.getTone() != null && !emailRequest.getTone().isEmpty()){
+            prompt.append("\n use a Tone: "+emailRequest.getTone());
+
+        }
     }
 }
