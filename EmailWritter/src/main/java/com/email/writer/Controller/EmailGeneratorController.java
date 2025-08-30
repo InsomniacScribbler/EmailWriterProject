949298772b3a5c5fc772f875ef83a229fe8587da
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-08-30 12:39:26 +0530

    BACKEND READY!!!!!

--- EmailWritter/src/main/java/com/email/writer/Controller/EmailGeneratorController.java
+++ EmailWritter/src/main/java/com/email/writer/Controller/EmailGeneratorController.java
@@ -1,15 +1,26 @@
 package com.email.writer.Controller;
 
 import com.email.writer.Entity.EmailRequest;
+import com.email.writer.Service.EmailGeneratorService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/email")
+@AllArgsConstructor
 public class EmailGeneratorController {
+
+    @Autowired
+    private final EmailGeneratorService emailGeneratorService;
+
+    @PostMapping("/generate")
     public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
-        return ResponseEntity.ok(emailResponse);
+        String response = emailGeneratorService.generateEmailRespone(emailRequest)  ;
+        return ResponseEntity.ok(response);
     }
 }
